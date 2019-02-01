110e129c98c05ae8106c2706d5b3829f939b7652
Bryan Robinson <bryanlrobinson@gmail.com>
2019-02-01 11:14:43 -0600

    Clears comment from data task

--- gulp-tasks/toolchest/get-posts.js
+++ gulp-tasks/toolchest/get-posts.js
@@ -12,43 +12,4 @@ gulp.task('get:posts', function () {
     // console.log(rss);
     project.storeData(project.buildSrc + "/site/_data/posts.json", JSON.stringify(rss));
   });
-  // axios.get('https://bryanlrobinson.com/feed.xml')
-  //   .then(function (response) {
-  //     console.log(response.data);
-  //     // var events = response.data.data[0].relationships.data;
-  //     // var eventURLs = [];
-  //     // events.forEach(element => {
-  //     //   eventURLs.push(element.links.event);
-  //     // });
-
-  //     // // Fetch all of the presentation data
-  //     // axios.all(eventURLs.map(l => axios.get(l)))
-  //     //   .then(axios.spread(function (...res) {
-
-  //     //     // gather the data about for each presentation and
-  //     //     // collect them in future and past arrays
-  //     //     var talks = {
-  //     //       future : [],
-  //     //       past : []
-  //     //     };
-  //     //     var now = new Date();
-  //     //     for (var talk in res) {
-  //     //       var thisTalk = res[talk].data.data[0].attributes;
-  //     //       var when = new Date(thisTalk.ends_on);
-  //     //       var future = now - when < 0 ? true : false;
-  //     //       if(future) {
-  //     //         talks.future.push(thisTalk);
-  //     //       } else {
-  //     //         talks.past.push(thisTalk);
-  //     //       }
-  //     //     }
-  //     //     // stash the data for the SSG to use
-  //     //     project.storeData(project.buildSrc + "/site/_data/events.json", JSON.stringify(talks));
-  //     //   })
-  //     // );
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
-
 });
