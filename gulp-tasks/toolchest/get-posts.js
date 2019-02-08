631032fd62d3317abb9845dc9a87f12697967dad
Bryan Robinson <bryanlrobinson@gmail.com>
2019-02-08 11:56:35 -0600

    removes typo from test

--- gulp-tasks/toolchest/get-posts.js
+++ gulp-tasks/toolchest/get-posts.js
@@ -8,7 +8,7 @@ const rss2json = require('rss-to-json');
   Get blog posts from main site
 */
 gulp.task('get:posts', async function () {
-  rss2json.load('https://bryanlrobinson.com/feed.dxml', function(err, rss){
+  rss2json.load('https://bryanlrobinson.com/feed.xml', function(err, rss){
     project.storeData(project.buildSrc + "/site/_data/posts.json", JSON.stringify(rss));
   });
 });
